41752513ee26197f1f60da8f692127893929cfe3
Uchechi Nwaka <nwakauc1@gmail.com>
2019-01-30 12:31:46 +0200

    console app debug

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Uche!");
+﻿public class Program
+{
+    public static int FindMax(int[] numbers)
+    {
+         if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("The array must contain at least one element.");
+        }
+        
+        int max = numbers[0];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public static void Main()
+    {
+        int[] myNumbers = { -5, -10, -3, -8, -2 };
+        int maxNumber = FindMax(myNumbers);
+        Console.WriteLine("The maximum number is: " + maxNumber);
+    }
+}
