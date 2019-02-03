1e69def3e0e56c966adf7d43805192d7d3b611c9
Uchechi Nwaka <nwakauc1@gmail.com>
2019-02-03 12:28:29 +0200

    console app debug

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,71 @@
-﻿int age = 23;
-Console.WriteLine("I am " + age + " years old.");
+﻿// using System.Security.Cryptography.X509Certificates;
 
-float pi = 3.14e30f;
-Console.WriteLine("Pi is approximately " + pi);
+public class TodoList {
+    
+    string[] tasks = new string[10];
+    int taskCount = 0;
+    public void AddTask() {
+         if (taskCount < tasks.Length) {
+         Console.Write("Enter a task: ");
+        string task = Console.ReadLine();
+        tasks[taskCount] = task;
+        taskCount++;
+            
+         } else {
+             Console.WriteLine("Task list is full.");
+        }
+        
+    }
+    public void ViewTasks(){
+            Console.WriteLine("Task List:");
+            for (int i = 0; i < taskCount; i++) {
+                Console.WriteLine($"{i + 1}. {tasks[i]}");
+            }
+           
+        }
+    public void CompleteTask(){
+        Console.Write("Enter the task number to complete: ");
+        int taskNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+        if (taskNumber >= 0 && taskNumber < taskCount) {
+            tasks[taskNumber] = null;
+            for (int i = taskNumber; i < taskCount - 1; i++) {
+                tasks[i] = tasks[i + 1];
+            }
+            taskCount--;
+            Console.WriteLine("Task completed.");
+        } else {
+            Console.WriteLine("Invalid task number.");
+        }
 
-double temperature = 37.5;
-Console.WriteLine("The temperature is " + temperature + " degrees.");
 
-char dollar = '$';
-Console.WriteLine("The dollar sign is " + dollar);
-
-string name = "John Doe";
-Console.WriteLine("My name is " + name);
+     
+    }   
+    public static void Main(string[] args) {
+        TodoList todoList = new TodoList();
+        while (true) {
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Tasks");
+            Console.WriteLine("3. Complete Task");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice) {
+                case 1:
+                    todoList.AddTask();
+                    break;
+                case 2:
+                    todoList.ViewTasks();
+                    break;
+                case 3:
+                    todoList.CompleteTask();
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
+   
