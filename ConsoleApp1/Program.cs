489357ca9dee0e4bb21a9f1871314fff72c07d86
Uchechi Nwaka <nwakauc1@gmail.com>
2019-03-06 11:38:03 +0200

    for loops

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,35 +2,63 @@
     class Program{
         static void Main(string[] args)
         {
-            Console.Write("Enter the number of the weekday: ");
-            int day = Convert.ToInt32(Console.ReadLine());
-            switch (day)
-            {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Invalid day you must enter a number between 1 and 7");
-                    break;
-            }
+
+            Console.WriteLine("Enter a word you want to repeat: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of times you want to repeat the word: ");
+            int times = Convert.ToInt32(Console.ReadLine());
+
+            
+                if (times == 0) {
+                    Console.WriteLine("Invalid number of times please enter a number more than 0");
+                } else {
+                    for (int i = 0; i <times; ++i){
+                        Console.WriteLine(word);
+                    }
+                }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            // Console.Write("Enter the number of the weekday: ");
+            // int day = Convert.ToInt32(Console.ReadLine());
+            // switch (day)
+            // {
+            //     case 1:
+            //         Console.WriteLine("Monday");
+            //         break;
+            //     case 2:
+            //         Console.WriteLine("Tuesday");
+            //         break;
+            //     case 3:
+            //         Console.WriteLine("Wednesday");
+            //         break;
+            //     case 4:
+            //         Console.WriteLine("Thursday");
+            //         break;
+            //     case 5:
+            //         Console.WriteLine("Friday");
+            //         break;
+            //     case 6:
+            //         Console.WriteLine("Saturday");
+            //         break;
+            //     case 7:
+            //         Console.WriteLine("Sunday");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Invalid day you must enter a number between 1 and 7");
+            //         break;
+            // }
 
 
  
