78b0cbea6a03b84685ed2f054697bbf202771c13
Uchechi Nwaka <nwakauc1@gmail.com>
2019-03-06 11:22:17 +0200

    switch case

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,30 +2,63 @@
     class Program{
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-
-            int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.Write("Enter the second number: ");
-
-            int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-            int answer = firstNumber * secondNumber;
-
-            Console.WriteLine("Value of " + firstNumber + " X " + secondNumber + " : " );
-            string answerString = Console.ReadLine();
-
-            int actualValue = Convert.ToInt32(answerString);
-
-            if (actualValue == answer)
+            Console.Write("Enter the number of the weekday: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
             {
-                Console.WriteLine("Correct!");
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day you must enter a number between 1 and 7");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Incorrect!");
-            }
+
+
+ 
+
+            // Console.Write("Enter the first number: ");
+
+            // int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            // Console.Write("Enter the second number: ");
+
+            // int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            // int answer = firstNumber * secondNumber;
+
+            // Console.WriteLine("Value of " + firstNumber + " X " + secondNumber + " : " );
+            // string answerString = Console.ReadLine();
+
+            // int actualValue = Convert.ToInt32(answerString);
+
+            // if (actualValue == answer)
+            // {
+            //     Console.WriteLine("Correct!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Incorrect!");
+            // }
 
 
 
