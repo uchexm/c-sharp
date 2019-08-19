765555c6276cd5005076bad58a7495b393fa72cc
Uchechi Nwaka <nwakauc1@gmail.com>
2019-08-19 06:31:31 +0200

    arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -176,6 +176,18 @@ namespace Exercise {
             
 
     // }
+             int[] numbers = new int[5];
+
+                numbers[0] = 10;
+                numbers[1] = 20;
+                numbers[2] = 30;
+                numbers[3] = 40;
+                numbers[4] = 50;
+
+                for (int i = 0; i < numbers.Length; ++i){
+                    Console.WriteLine(numbers[i]);  
+                }
+            
 
 
 
