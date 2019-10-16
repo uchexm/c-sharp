409b0c77cf92e8c4795f20da1e47c8bde4f407be
Uchechi Nwaka <nwakauc1@gmail.com>
2019-10-16 17:19:35 +0200

    calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,22 +3,30 @@
 namespace Exercise {
     class Program{
         static void Main(string[] args){
-            int width = ReadInt("Enter the width: ");
-            int height = ReadInt("Enter the height: ");
+            
+        int[] numbers = new int[]{ 4, 2, 6, 1, 3, 5 };
+        if (numbers.Length > 0) {
+            Console.WriteLine("Sum of the numbers: " + SumOfNumbers(numbers));
 
-            int result = CalculateArea(width, height);
-            Console.WriteLine($"The area is: {result}");
-
-       }
-        static int CalculateArea(int width, int height){
-            return (width * height) / 2;
+        }else{
+             Console.Write("The array of numbers is empty");
         }
-
-        static int ReadInt(string message){
-            Console.Write(message);
-            return int.Parse(Console.ReadLine());
+        
         }
-    
+       
+     
+
+      static int SumOfNumbers(int[] numbers) {
+        if (numbers.Length > 0) {
+            int sum = 0;
+        foreach (int num in numbers) {
+          sum += num;
+        }
+        return sum;
+
+        }
+        return -1;
+      }
     
     }
 
