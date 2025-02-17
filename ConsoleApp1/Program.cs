52a0622eda5d697f969fcc8061677776b19aa4a9
Uchechi Nwaka <nwakauc1@gmail.com>
2025-02-17 13:36:50 +0200

    inheritance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,48 @@
 ﻿using System.Formats.Asn1;
 
 namespace Exercise {
-    class Program{
-        static void Main(string[] args){
-            
-        int[] numbers = new int[]{ 4, 2, 6, 1, 3, 5 };
-        if (numbers.Length > 0) {
-            Console.WriteLine("Sum of the numbers: " + SumOfNumbers(numbers));
+  public interface IAnimal
+{
+    void Eat();
+}
 
-        }else{
-             Console.Write("The array of numbers is empty");
-        }
-        
-        }
-       
-     
-
-      static int SumOfNumbers(int[] numbers) {
-        if (numbers.Length > 0) {
-            int sum = 0;
-        foreach (int num in numbers) {
-          sum += num;
-        }
-        return sum;
-
-        }
-        return -1;
-      }
-    
+public class Animal : IAnimal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound");
     }
 
-};
+    public void Eat()
+    {
+        Console.WriteLine("Some generic animal food");
+    }
+}
+
+public class Dog : Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Kibble");
+    }
+
+    public override void MakeSound(){
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Tuna");
+    }
+
+    public override void MakeSound(){
+        Console.WriteLine("Meow");
+    }
+}
 
 
-       
+
+}
